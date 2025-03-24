bc9ca55e33844de6f96cb4282f725817efaf3524
Syrex310 <duongrobloxvn@gmail.com>
2025-03-24 15:11:27 +0700

    enemy

--- main.cpp
+++ main.cpp
@@ -1,30 +1,56 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
+using namespace std;
 struct Player {
     int x, y;
     int width, height;
     int speed;
 };
 
+struct Enemy {
+    int x, y;
+    int width, height;
+    int speed;
+
+    void moveTowardPlayer(Player& player) {
+        int dx = player.x - x;
+        int dy = player.y - y;
+    
+        float length = sqrt(dx * dx + dy * dy);
+        if (length != 0) {
+            float normalizedX = dx / length;
+            float normalizedY = dy / length;
+    
+            x += round(normalizedX * speed);
+            y += round(normalizedY * speed);
+        }
+    }
+    
+};
+
+vector<Enemy> enemies;
+
 bool init(SDL_Window*& window, SDL_Renderer*& renderer) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
-        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
         return false;
     }
 
     window = SDL_CreateWindow("SDL Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
-        std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
         return false;
     }
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
-        std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
         return false;
     }
 
@@ -48,11 +74,28 @@ void updatePlayerMovement(Player& player) {
     if (keystate[SDL_SCANCODE_S]) player.y += player.speed;
 }
 
+void spawnEnemy() {
+    Enemy enemy;
+    enemy.x = rand() % SCREEN_WIDTH;
+    enemy.y = rand() % SCREEN_HEIGHT;
+    enemy.width = 40;
+    enemy.height = 40;
+    enemy.speed = 2;
+    enemies.push_back(enemy);
+}
 
 void renderPlayer(SDL_Renderer* renderer, Player& player) {
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Green player
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); 
     SDL_Rect rect = { player.x, player.y, player.width, player.height };
     SDL_RenderFillRect(renderer, &rect);
+}
+
+void renderEnemies(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    for (auto& enemy : enemies) {
+        SDL_Rect rect = { enemy.x, enemy.y, enemy.width, enemy.height };
+        SDL_RenderFillRect(renderer, &rect);
+    }
 }
 
 void gameLoop(SDL_Window* window, SDL_Renderer* renderer) {
@@ -61,22 +104,40 @@ void gameLoop(SDL_Window* window, SDL_Renderer* renderer) {
 
     Player player = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 50, 50, 5 };
 
+    for (int i = 0; i < 5; i ++){
+        spawnEnemy();
+    }
+
     const int FPS = 60;
-    const int frameDelay = 1000 / FPS; // 16 milliseconds per frame
+    const int frameDelay = 1000 / FPS; 
 
     Uint32 frameStart;
     int frameTime;
 
+    Uint32 lastSpawnTime = SDL_GetTicks();
+    const int spawnInterval = 5000;
+
     while (running) {
-        frameStart = SDL_GetTicks(); // Get time at start of frame
+        frameStart = SDL_GetTicks();
 
         handleInput(event, running);
         updatePlayerMovement(player);
+        
+        if (SDL_GetTicks() - lastSpawnTime > spawnInterval) {
+            spawnEnemy();
+            lastSpawnTime = SDL_GetTicks();
+        }
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black background
+        for (auto& enemy : enemies) {
+            enemy.moveTowardPlayer(player);
+        }
+        
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
         SDL_RenderClear(renderer);
 
         renderPlayer(renderer, player);
+        renderEnemies(renderer);
 
         SDL_RenderPresent(renderer);
 
